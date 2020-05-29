31bbd4c763a7fcd6ac5e6774259510028c48adb6
Katell Goaer <alt.katellgoaer@gmail.com>
2020-05-29 18:21:50 -0400

    implemented method to remove ingredient

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,7 @@ import classes from './BuildControl.css';
 const BuildControl = (props) => (
   <div className={classes.buildControl}>
     <div className={classes.label}>{props.label}</div>
-    <button className={classes.less}>Less</button>
+    <button className={classes.less} onClick={props.removed}>Less</button>
     <button className={classes.more} onClick={props.added}>More</button>
   </div>
 );

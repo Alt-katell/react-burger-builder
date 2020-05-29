ebd12f8a9c5f3c1315fe6c54d00da49fbeafbb7b
Katell Goaer <alt.katellgoaer@gmail.com>
2020-05-29 11:47:05 -0400

    created hardcoded version of a burger

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import classes from './Burger.css';
+
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+const Burger = (props) => {
+  return (
+    <div className={classes.burger}>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  )
+}
+
+export default Burger;

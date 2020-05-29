a6a56a410d5f13d69066977c06f3704f2a1fdc61
Katell Goaer <alt.katellgoaer@gmail.com>
2020-05-29 16:01:49 -0400

    added possibilty to add ingredient with BuildControl

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,7 +6,7 @@ const BuildControl = (props) => (
   <div className={classes.buildControl}>
     <div className={classes.label}>{props.label}</div>
     <button className={classes.less}>Less</button>
-    <button className={classes.more}>More</button>
+    <button className={classes.more} onClick={props.added}>More</button>
   </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
-const BuildControls = (props) => (
-  <div>
+import classes from './BuildControls.css';
 
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+]
+
+const BuildControls = (props) => (
+  <div className={classes.buildControls}>
+    {controls.map(control => (
+      <BuildControl
+        key={control.label}
+        label={control.label}
+        added={() => props.ingredientAdded(control.type)}
+        removed={() => props.ingredientRemoved(control.type)} />
+    ))}
   </div>
 );
 

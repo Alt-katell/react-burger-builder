4cbab71ec0f4603900a592669f1cce4834fec854
Katell Goaer <alt.katellgoaer@gmail.com>
2020-05-29 12:25:03 -0400

    outputted burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,18 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const Burger = (props) => {
+  let ingredientList = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])]  //created one space in this new array every time it found ingredientKey in the props.ingredients object
+      .map((_, i) => {
+        return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
+      });
+    });
+
   return (
     <div className={classes.burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredientList}
       <BurgerIngredient type="bread-bottom" />
     </div>
   )

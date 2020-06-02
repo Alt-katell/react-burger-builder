e6bc71dffd13b348d5fee58f2da9470a87f8bd9f
Katell Goaer <alt.katellgoaer@gmail.com>
2020-06-02 14:30:36 -0400

    added a hamburger menu icon for mobile

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import classes from './DrawerToggle.css';
+
 const DrawerToggle = (props) => (
-  <div onClick={props.clicked}>MENU</div>
+  <div
+    onClick={props.clicked}
+    className={classes.drawerToggle}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 );
 
 export default DrawerToggle;

5d97c6c56f0c845cfe51171e2f0c6d51497ca5d9
Katell Goaer <alt.katellgoaer@gmail.com>
2020-06-02 12:17:39 -0400

    reused backdrop for when sidedrawer is open

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
@@ -6,16 +6,28 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.css';
 
-const Layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.content}>
-      {props.children}
-    </main>
-  </Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
 
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}/>
+        <main className={classes.content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,20 +2,32 @@ import React from 'react';
 
 import classes from './SideDrawer.css';
 
+import Aux from '../../../hoc/Aux';
+
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const SideDrawer = (props) => {
+  let attachedClasses = [classes.sideDrawer, classes.close];
+  if (props.open) {
+    attachedClasses = [classes.sideDrawer, classes.open];
+  }
 
   return(
-    <div className={classes.sideDrawer}>
-      <div className={classes.logo}>
-        <Logo />
+    <Aux>
+      <Backdrop
+        show={props.open}
+        clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 }
 

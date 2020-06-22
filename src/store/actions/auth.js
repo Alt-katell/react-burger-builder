7a5054eb6bab1893795227b792f55d20d1b32a63
Katell Goaer <alt.katellgoaer@gmail.com>
2020-06-21 21:25:35 -0400

    fixed issue with api key

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -45,9 +45,11 @@ export const auth = (email, password, isSignup) => {
       password: password,
       returnSecureToken: true
     }
-    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API}`;
+    let apiKey = process.env.REACT_APP_FIREBASE_API
+    console.log(apiKey)
+    let url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`;
     if (!isSignup) {
-      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API}`;
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`;
     }
     axios.post(url, authData)
       .then(response => {
